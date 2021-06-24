6f38c2dd3b74e764912ea4ff7d701024d5c24339
Jason Yue <kymotsujason@gmail.com>
2021-06-23 22:39:33 -0700

    Commit files
    - Added files

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-cloudflare
+# Cloudflare Zones DNS Updater
+
+A simple NodeJS script that checks and updates the IPs for each DNS in the Zones. Intended for home users where every zone's DNS has the same IP. Feel free to modify it for your use case (ie. different DNS types, multi-IP configs, etc).
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running.
+
+### Prerequisites
+
+What things you need to utilize this project and how to install them
+
+```
+NodeJS
+Cloudflare Account
+CloudFlare API Token
+```
+
+### Installing
+
+A step by step series of examples that tell you how to get a development env running
+
+
+```
+1. Clone the repo
+2. npm install
+3. Run with your favorite process manager (I use PM2)
+```
+
+
+
+## Running the tests
+
+N/A, this is too simple
+
+## Deployment
+
+Same as installation, it can run in a development and production environment.
+
+## Built With
+
+* [NodeJS](https://nodejs.org/en/) - The runtime environment being used
+
+## Contributing
+
+Please read [CONTRIBUTING.md](CONTRIBUTING.md) for details on our code of conduct, and the process for submitting pull requests to us.
+
+## Versioning
+
+None, shouldn't need any.
+
+## Authors
+
+* **Jason Yue** - *Initial work* - [kymotsujason](https://github.com/kymotsujason)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* Cloudflare for their amazing API
+* Telus for changing my public IP whenever my router restarts, which is why I made this
+
---
+++ index.js
@@ -0,0 +1,66 @@
+const dotenv = require("dotenv");
+dotenv.config();
+var CronJob = require("cron").CronJob;
+const express = require("express");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+const publicIp = require("public-ip");
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+let url = "https://api.cloudflare.com/client/v4/zones/";
+axios.defaults.headers.common["Content-Type"] = "application/json";
+axios.defaults.headers.common["Authorization"] = "Bearer " + process.env.AUTH;
+
+async function Main() {
+    let oldIp = "";
+    let newIp = await publicIp.v4({ onlyHttps: true });
+    let zones = [];
+    await axios
+        .get(url)
+        .then((res) => {
+            for (let i = 0; i < res.data.result.length; i++) {
+                zones.push(res.data.result[i].id);
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    for (let i = 0; i < zones.length; i++) {
+        await axios.get(url + zones[i] + "/dns_records").then((res) => {
+            oldIp = res.data.result[0].content;
+            if (newIp === oldIp) {
+                console.log("IPs has not changed.");
+                return;
+            } else {
+                for (let j = 0; j < res.data.result.length; j++) {
+                    axios.put(
+                        url +
+                            zones[i] +
+                            "/dns_records/" +
+                            res.data.result[j].id,
+                        {
+                            type: "A",
+                            name: res.data.result[j].name,
+                            content: newIp,
+                            ttl: 1,
+                            proxied: res.data.result[j].proxied,
+                        }
+                    );
+                }
+            }
+        });
+    }
+}
+
+var job = new CronJob(
+    "0 */1 * * * *",
+    function () {
+        Main();
+    },
+    null,
+    true,
+    "America/Vancouver"
+);
+job.start();
